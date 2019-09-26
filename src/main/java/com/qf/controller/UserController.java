a19d33df68a82ecd724f2ad242061a23d489d265
coderhappynoob <55830196+coderhappynoob@users.noreply.github.com>
2019-09-26 19:15:32 +0800

    Update UserController.java

--- src/main/java/com/qf/controller/UserController.java
+++ src/main/java/com/qf/controller/UserController.java
@@ -3,5 +3,6 @@ package com.qf.controller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("github");
     }
 }
